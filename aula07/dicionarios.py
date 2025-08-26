12a3c91ee696f7a02d9b626ec6dd01a9a01cedc0
aghown <hannasilva696@gmail.com>
2025-08-26 19:03:56 +0000

    aula sobre dicionarios

---
+++ aula07/dicionarios.py
@@ -0,0 +1,16 @@
+#criando um dicionario
+dicionario_vazio = {}
+print(type(dicionario_vazio))
+
+dicionario = {
+    "nome": "Fred",
+    "Idade": 30,
+    "Nacionalidade": "brasileiro",
+    "Rico":False
+}
+
+print(dicionario)
+
+#Acessando itens
+
+print(dicionario["nome"])
