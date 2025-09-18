1b1b5113dac75b7263c8616c566389fe55780d9a
aghown <hannasilva696@gmail.com>
2025-09-18 19:57:19 +0000

    aula e exercicios de POO: herança

---
+++ orientacao_objeto/aula_03_herança.py
@@ -0,0 +1,12 @@
+class Mamifero:
+    def __init__(self, especie, habitat):
+        self.especie = especie
+        self.habitat = habitat
+
+    def __str__(self):
+        return
+    
+    def movimentar(self):
+        ...
+class Cachorro(Mamifero):
+    ...
